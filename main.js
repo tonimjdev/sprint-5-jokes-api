fcf4d4123ab8287a6b5d976826ce173a2be8a932
tonimjdev <tonimdev@gmail.com>
2022-09-10 17:12:15 +0200

    Exercici 3 - Score Jokes

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 "use strict";
-// Arxiu Typescript de l'Sprint 5 (Itinerari ANGULAR) de l'alumne Toni Machado
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,6 +8,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// Arxiu Typescript de l'Sprint 5 (Itinerari ANGULAR) de l'alumne Toni Machado
+// Guardem data amb format ISO
+const d = new Date();
+let dateToday = d.toISOString();
+console.log(dateToday);
+// Declarem array pels reports
+const reportAcudits = [];
+// Declarem variable per l'acudit rebut de l'API
+let acudit = "";
+// Funció per amagar element del DOM
+const hideElement = (idElement) => {
+    let elementToHide = document.getElementById(idElement);
+    elementToHide.style.display = "none";
+};
+// Funció per mostrar element del DOM
+const showElement = (idElement) => {
+    let elementToShow = document.getElementById(idElement);
+    elementToShow.style.display = "inline";
+};
+// Funció per rebre l'acudit (GET JOKE) amb funció asincrona i fetch
 function getJoke() {
     const carregarAcudit = () => __awaiter(this, void 0, void 0, function* () {
         try {
@@ -23,14 +42,30 @@ function getJoke() {
             // Check resposta
             console.log(datos);
             // Extraiem acudit
-            let acudit = datos.joke;
+            acudit = datos.joke;
             // Mostrem per pantalla
             let acuditHTML = document.getElementById('acudit');
             acuditHTML.innerHTML = `<cite>"${acudit}"</cite>`;
+            showElement('negativeButton');
+            showElement('neutralButton');
+            showElement('positiveButton');
         }
         catch (error) {
             console.log(error);
         }
     });
     carregarAcudit();
+}
+// Funcio per anar guardant puntuacions dels acudits en un array
+function scoreButton(score) {
+    let report = {
+        joke: acudit,
+        score: score,
+        date: dateToday,
+    };
+    reportAcudits.push(report);
+    console.log(reportAcudits);
+    hideElement('negativeButton');
+    hideElement('neutralButton');
+    hideElement('positiveButton');
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
 // Arxiu Typescript de l'Sprint 5 (Itinerari ANGULAR) de l'alumne Toni Machado
+// Guardem data amb format ISO
+const d = new Date();
+let dateToday:string = d.toISOString();
+console.log (dateToday);
 
+// Declarem array pels reports
+const reportAcudits: { joke:string; score:number; date:string }[] = [];
+// Declarem variable per l'acudit rebut de l'API
+let acudit:string="";
+
+// Funció per amagar element del DOM
+const hideElement = (idElement:string) => {
+    let elementToHide = document.getElementById(idElement) as HTMLElement;
+    elementToHide.style.display = "none";
+}
+
+// Funció per mostrar element del DOM
+const showElement = (idElement:string) => {
+    let elementToShow = document.getElementById(idElement) as HTMLElement;
+    elementToShow.style.display = "inline";
+}
+
+
+// Funció per rebre l'acudit (GET JOKE) amb funció asincrona i fetch
 function getJoke() {
     const carregarAcudit = async() => {
         try {
@@ -14,13 +37,31 @@ function getJoke() {
         // Check resposta
         console.log(datos);
         // Extraiem acudit
-        let acudit = datos.joke;
+        acudit = datos.joke;
         // Mostrem per pantalla
         let acuditHTML = document.getElementById('acudit') as HTMLElement;
         acuditHTML.innerHTML = `<cite>"${acudit}"</cite>`;
+        showElement('negativeButton');
+        showElement('neutralButton');
+        showElement('positiveButton');
     } catch(error) {
         console.log(error);
     }
 }
 carregarAcudit();
 }
+// Funcio per anar guardant puntuacions dels acudits en un array
+function scoreButton(score:number) {
+    let report = {
+        joke: acudit,
+        score: score,
+        date: dateToday,
+    }
+    reportAcudits.push(report);
+    console.log(reportAcudits);
+    hideElement('negativeButton');
+    hideElement('neutralButton');
+    hideElement('positiveButton');
+}
+
+
